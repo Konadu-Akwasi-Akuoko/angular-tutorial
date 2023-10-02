a85e78da999e38391ebcefb3efc6b044c8587000
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2023-10-02 09:18:34 +0000

    Update: Learned how to modify angular components, and angular view files

--- AngularBasics/src/app/app.component.ts
+++ AngularBasics/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AngularBasics';
+  name = 'Akwasi Konadu Akuoko';
+  age: number = 54;
+  getNameAndAge = () => {
+    return `My name is ${this.name} and I am ${this.age} years old`;
+  };
 }
